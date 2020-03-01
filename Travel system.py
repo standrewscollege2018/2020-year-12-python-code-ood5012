5d3132978afe658004227d7cea5de6feb05cc842
ood5012 <ood5012@stacmail.net>
2020-03-02 12:05:32 +1300

    Book flight
    Can book flights and accommodation and get a price for the trip

--- Travel system.py
+++ Travel system.py
@@ -9,3 +9,11 @@ locations = [["Sydney",326,120],["Tonga",378,40],["Shanghai",1436,60],["London",
 
 for location in range(0,len(locations)):
     print(location+1,locations[location][0])
+#Input for choosing what place you would like to travel to
+location_choice = int(input("Where would you like to travel(use index number)?"))
+nights_stay = int(input("How many nights would you like to stay?"))
+#Calculates price based on place and nights and then prints it
+price_flights = locations[location_choice-1][1]
+price_accommodation = locations[location_choice-1][2]*nights_stay
+price = price_flights + price_accommodation
+print (price)
